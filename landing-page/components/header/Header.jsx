75de60f88e368b9a6ef35783cb4c749809d1bf6a
carloscordova.dev <cordovamanuel22@icloud.com>
2024-05-31 07:47:18 -0700

    add nav links

---
+++ landing-page/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '@/components/nav/Nav'
+const Header = () => {
+  return (
+    <header className='bg-blue-200'>
+      <Nav />
+      <h1>Header</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ landing-page/components/nav/Nav.jsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import React from 'react'
+const nav = () => {
+  return (
+    <>
+      <nav id='desktop-nav' className='hidden md:flex justify-between bg-transparent mx-6'>
+        <section>
+          <Link href='#'>
+            <h1>sunnyside</h1>
+          </Link>
+        </section>
+        <section>
+          <ul className='flex  gap-4'>
+            <li>
+              <Link href='#'>About</Link>
+            </li>
+            <li>
+              <Link href='#'>Services</Link>
+            </li>
+            <li>
+              <Link href='#'>Projects</Link>
+            </li>
+            <li>
+              <Link href='#'>Contact</Link>
+            </li>
+          </ul>
+        </section>
+      </nav>
+      <nav id='mobile-nav' className=' flex md:hidden'>
+        <h1>MOBILE</h1>
+      </nav>
+
+    </>
+  )
+}
+
+export default nav
